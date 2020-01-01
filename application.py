226d60ff1d076e66f32b8cdbf5c72cda1648a561
Jeremy Silver <jeremyag@comcast.net>
2020-01-01 16:05:43 -0500

    Made Smash app.

--- application.py
+++ application.py
@@ -1,30 +1,29 @@
 from flask import Flask
 
+from rand import random_melee_char
+
+
 # print a nice greeting.
-def say_hello(username = "World"):
-    return '<p>Hello %s!</p>\n' % username
+def random_char():
+    char = random_melee_char()
+    return f'<p>{char}</p>\n'
 
 # some bits of text for the page.
 header_text = '''
-    <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
-instructions = '''
-    <p><em>Hint</em>: This is a RESTful web service! Append a username
-    to the URL (for example: <code>/Thelonious</code>) to say hello to
-    someone specific.</p>\n'''
-home_link = '<p><a href="/">Back</a></p>\n'
-footer_text = '</body>\n</html>'
+    <html>\n<head> <title>Random Melee Character</title> </head>\n<body><h1 align="center">'''
+footer_text = '</h1></body>\n</html>'
 
 # EB looks for an 'application' callable by default.
 application = Flask(__name__)
 
 # add a rule for the index page.
 application.add_url_rule('/', 'index', (lambda: header_text +
-    say_hello() + instructions + footer_text))
+    random_char() + footer_text))
 
-# add a rule when the page is accessed with a name appended to the site
-# URL.
-application.add_url_rule('/<username>', 'hello', (lambda username:
-    header_text + say_hello(username) + home_link + footer_text))
+# # add a rule when the page is accessed with a name appended to the site
+# # URL.
+# application.add_url_rule('/<username>', 'hello', (lambda username:
+#     header_text + say_hello(username) + home_link + footer_text))
 
 # run the app.
 if __name__ == "__main__":

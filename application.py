0f014dc86708927109d0bc4206b8438bbab42a47
Jeremy Silver <jeremyag@comcast.net>
2020-01-01 16:11:42 -0500

    Changed hostname to 0.0.0.0.

--- application.py
+++ application.py
@@ -30,4 +30,4 @@ if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
     #application.debug = True
-    application.run()
+    application.run('0.0.0.0')
